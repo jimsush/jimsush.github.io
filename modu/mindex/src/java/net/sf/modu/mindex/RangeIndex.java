5961bd46173c8dedcae08b6785efcccc415167ea
jimsush <jimsu123@gmail.com>
2013-12-17 20:17:53 +0800

    Update RangeIndex.java

--- modu/mindex/src/java/net/sf/modu/mindex/RangeIndex.java
+++ modu/mindex/src/java/net/sf/modu/mindex/RangeIndex.java
@@ -1,44 +1,131 @@
-/**
- * 
- */
 package net.sf.modu.mindex;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.SortedMap;
 import java.util.TreeMap;
 import java.util.concurrent.locks.ReadWriteLock;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
 
-/**
- * @author tong
- *
- */
 public class RangeIndex {
-	private ReadWriteLock rwlock=new ReentrantReadWriteLock();
-	private TreeMap<Object, Set<Object>> valuesOfIndex=new TreeMap<Object, Set<Object>>();
 	
-	public int getValuesNumber(){
-		return valuesOfIndex.size();
+	/** Not Null Index */
+	private TreeMap<Object,Map> valuesToEntries=new TreeMap<Object,Map>();
+	
+	/** handle null index */
+	private Map nullValuesToEntries=new HashMap();
+	
+	private ReadWriteLock rwLock=new ReentrantReadWriteLock();
+	
+	public int getIdenticalIndexCount(){
+		return valuesToEntries.size()+1;
 	}
 	
-	public void addEntry(Object indexValue,Object entry){
-		Set<Object> values=valuesOfIndex.get(indexValue);
-		if(values==null){
-			values=new HashSet<Object>();
-			valuesOfIndex.put(indexValue, values);
+	public void clear(){
+		valuesToEntries.clear();
+		nullValuesToEntries.clear();
+	}
+	
+	public void addEntry(Object indexValue,EntryHolder entry){
+		if(indexValue==null){
+			nullValuesToEntries.put(entry.getPrimaryKey(), entry.getValue());
+		}else{
+			Map subEntries = valuesToEntries.get(indexValue); // index value
+			if(subEntries==null){
+				subEntries=new HashMap();
+				valuesToEntries.put(indexValue, subEntries);
+			}
+			subEntries.put(entry.getPrimaryKey(), entry.getValue());
 		}
-		values.add(entry); // hashCode(???), 减少hashCode的计算，到底放entry还是primary Key了?
-		
-		// why create hashset if just one entry in set?  (to optimize later)
 	}
 	
-	public void remove(Object indexValue,Object key){
-		Set<Object> values=valuesOfIndex.get(indexValue);
-		
+	public void removeEntry(Object indexValue,Object primaryKey){
+		if(indexValue==null){
+			nullValuesToEntries.remove(primaryKey);
+		}else{
+			Map subEntries = valuesToEntries.get(indexValue); 
+			if(subEntries!=null)
+				subEntries.remove(primaryKey);
+		}
 	}
 	
-	public void query(int operator,Object indexValue){
+	public List query(Op op, Object conditionValue){
+		List result=new ArrayList();
+		if(conditionValue==null){
+			if(op==Op.EQ){
+				result.addAll(nullValuesToEntries.values());
+			}else if(op==Op.NE){
+				for(Map.Entry<Object, Map> indexEntry : valuesToEntries.entrySet()){
+					Map<Object,Object> m=(Map<Object,Object>)indexEntry.getValue();
+					for(Map.Entry entry : m.entrySet()){
+						result.add(entry.getValue());
+					}
+				}
+			}else{
+				throw new IllegalArgumentException("Don't support "+op+" for null");
+			}
+		}else{
+			boolean inclusive=true;
+			switch(op){
+			case EQ:
+				Map map = valuesToEntries.get(conditionValue);
+				if(map!=null){
+					Map<Object,Object> m=(Map<Object,Object>)map;
+					for(Map.Entry entry : m.entrySet()){
+						result.add(entry.getValue());
+					}
+				}
+				break;
+				
+			case NE: // TODO the below implementation is not good, it is still a full entry scan
+				for(Map.Entry<Object, Map> indexEntry : valuesToEntries.entrySet()){
+					Object idxKey=indexEntry.getKey();
+					if(!conditionValue.equals(idxKey)){ // exclude
+						Map<Object,Object> m=(Map<Object,Object>)indexEntry.getValue();
+						for(Map.Entry entry : m.entrySet()){
+							result.add(entry.getValue());
+						}
+					}
+				}
+				break;
+				
+			case LT:
+				inclusive=false;
+			case LE:
+				SortedMap<Object, Map> subMap = valuesToEntries.headMap(conditionValue,inclusive);
+				if(subMap!=null){
+					for(Map.Entry<Object, Map> indexEntry : subMap.entrySet()){
+						Map<Object,Object> m=(Map<Object,Object>)indexEntry.getValue();
+						for(Map.Entry entry : m.entrySet()){
+							result.add(entry.getValue());
+						}
+					}
+				}
+				break;
+				
+			case GT:
+				inclusive=false;
+			case GE:
+				SortedMap<Object, Map> subMap2 = valuesToEntries.tailMap(conditionValue,inclusive);
+				if(subMap2!=null){
+					for(Map.Entry<Object, Map> indexEntry : subMap2.entrySet()){
+						Map<Object,Object> m=(Map<Object,Object>)indexEntry.getValue();
+						for(Map.Entry entry : m.entrySet()){
+							result.add(entry.getValue());
+						}
+					}
+				}
+				break;
+				
+			default:
+				throw new IllegalArgumentException("Don't support "+op);
+			}
 		
+		}
+		
+		return result;
 	}
 	
 }
